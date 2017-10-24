dad5fe0720041e7eb271e8b505d4ff5ca4818a94
gdluck <gdluck256@gmail.com>
2017-10-24 16:36:09 +0800

    测试

--- backend/components/ArticleRule.php
+++ backend/components/ArticleRule.php
@@ -1,17 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: G-emall
- * Date: 2017/10/24
- * Time: 14:50
- */
-
 namespace backend\components;
 
 use yii;
 use yii\rbac\Rule;
 
-class ArticleRule
-{
 
+class ArticleRule extends Rule
+{
+    public $name = 'article';
+    public function execute($user, $item, $params)
+    {
+        // 这里先设置为false,逻辑上后面再完善
+        return false;
+    }
 }
