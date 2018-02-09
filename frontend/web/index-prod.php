314303c2ff7b0d4bd8d91cefbe9c39a82aa5f615
gdluck <gdluck256@gmail.com>
2018-02-09 17:18:39 +0800

    生产配置使用方法修改

--- frontend/web/index-prod.php
+++ frontend/web/index-prod.php
@@ -8,7 +8,9 @@ require(__DIR__ . '/../config/bootstrap.php');
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../common/config/main.php'),
-    require(__DIR__ . '/../config/main.php')
+    require(__DIR__ . '/../../common/config/main-prod.php'),
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main-prod.php')
 );
 
 (new yii\web\Application($config))->run();
